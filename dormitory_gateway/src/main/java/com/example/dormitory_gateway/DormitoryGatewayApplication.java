54ce55edc948b1502318cdaf258e26ebaf07575a
Artur <artur.spiewak2967@gmail.com>
2023-11-12 22:57:03 +0100

    Create gateway

---
+++ dormitory_gateway/src/main/java/com/example/dormitory_gateway/DormitoryGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.dormitory_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormitoryGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormitoryGatewayApplication.class, args);
+	}
+
+}
