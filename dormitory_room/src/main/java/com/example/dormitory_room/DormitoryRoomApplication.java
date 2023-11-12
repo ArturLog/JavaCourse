a82fa79c2dc3cea666a633747590d7c7724c1593
Artur <artur.spiewak2967@gmail.com>
2023-11-12 20:59:38 +0100

    init two separate projects

---
+++ dormitory_room/src/main/java/com/example/dormitory_room/DormitoryRoomApplication.java
@@ -0,0 +1,13 @@
+package com.example.dormitory_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormitoryRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormitoryRoomApplication.class, args);
+	}
+
+}
---
+++ dormitory_student/src/main/java/com/example/dormitory_student/DormitoryStudentApplication.java
@@ -0,0 +1,13 @@
+package com.example.dormitory_student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormitoryStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DormitoryStudentApplication.class, args);
+	}
+
+}
